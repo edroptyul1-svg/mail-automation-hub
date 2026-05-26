761e578a25971e166b24d70f3d1dbcdbc6f9d477
edroptyul1-svg <edroptyul1@gmail.com>
2026-05-26 13:00:47 +0700

    add main application

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Email automation hub with template management and scheduling"""
+
+class App:
+    def __init__(self):
+        self.config = {}
+    
+    def run(self):
+        print("Running mail-automation-hub...")
+        return True
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
